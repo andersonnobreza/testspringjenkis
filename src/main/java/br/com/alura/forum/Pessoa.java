b22b6910fc20eafb902d7f1f01b1aeaf17d69c1f
andersonnobreza <anderson.nobreza@gmail.com>
2022-02-23 12:01:22 -0300

    Add pessoa

---
+++ src/main/java/br/com/alura/forum/Pessoa.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum;
+
+public class Pessoa {
+
+	private String nome;
+	private String idade;
+	
+	public Pessoa(String nome, String idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+}
